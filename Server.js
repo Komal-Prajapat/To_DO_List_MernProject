724e3b010721146b0add47893a5842e1c01ff062
komalcod <128589948+komalcod@users.noreply.github.com>
2023-12-23 16:31:11 +0530

    add cors

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ app.use(bodyparser.json());
 
 
 app.use(cors({
-    origin:"http://localhost:5174/",
+    origin:"http://localhost:5173",
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
